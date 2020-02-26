71aaf0816514bcbba086953686e88fdaae9c1a81
Taha Yasin Erol <tahayasinerol@mizmer.com.tr>
2020-02-26 17:58:42 +0300

    Add project files.

---
+++ Deneme/Context/EmployeeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Deneme.Context
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options) { }
+        DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee() { EmployeeId = 1, Name = "Osman", Designation = "Developer", Address = "Ankara", CompanyName = "Mizmer" },
+                new Employee() { EmployeeId = 2, Name = "Furkan", Designation = "Avukat", Address = "Kayseri", CompanyName = "Baro" }
+            );
+        }
+    }
+}
---
+++ Deneme/Migrations/20200226144750_seeddata.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Deneme.Migrations
+{
+    public partial class seeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "Address", "CompanyName", "Designation", "Name", "Salary" },
+                values: new object[] { 1, "Ankara", "Mizmer", "Developer", "Osman", 2f });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "Address", "CompanyName", "Designation", "Name", "Salary" },
+                values: new object[] { 2, "Kayseri", "Baro", "Avukat", "Furkan", 4f });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 2);
+        }
+    }
+}
